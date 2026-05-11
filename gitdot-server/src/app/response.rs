cc0dd85e3d333cda0317b7eb675944c6c765ff05
baepaul <paul@gitdot.io>
2026-05-11 09:28:39 +0900

    fixing 502 error for status code

--- gitdot-server/src/app/response.rs
+++ gitdot-server/src/app/response.rs
@@ -17,6 +17,9 @@ impl<T: ApiResource> AppResponse<T> {
 
 impl<T: ApiResource> IntoResponse for AppResponse<T> {
     fn into_response(self) -> Response {
+        if matches!(self.0, StatusCode::NO_CONTENT | StatusCode::NOT_MODIFIED) {
+            return self.0.into_response();
+        }
         (self.0, Json(self.1)).into_response()
     }
 }
