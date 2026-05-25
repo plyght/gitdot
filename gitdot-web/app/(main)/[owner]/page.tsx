2988b1c3a5f4d9bc332be9f19b862c8e181a35d9
baepaul <paul@gitdot.io>
2026-05-25 13:12:19 -0400

    owner not found center page

--- gitdot-web/app/(main)/[owner]/page.tsx
+++ gitdot-web/app/(main)/[owner]/page.tsx
@@ -1,4 +1,6 @@
 import { getOrganization, getUser } from "@/dal";
+import Link from "@/ui/link";
+
 import OrgPage from "./ui/org/org-page";
 import UserPage from "./ui/user/user-page";
 
@@ -22,5 +24,15 @@ export default async function Page({
   if (user) return <UserPage user={user} />;
   if (org) return <OrgPage org={org} />;
 
-  return <div className="p-2 text-sm font-mono">{owner} not found</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full gap-1 p-4">
+      <p className="text-sm font-mono text-foreground">{owner} not found</p>
+      <Link
+        href={"/"}
+        className="text-xs text-muted-foreground underline lowercase"
+      >
+        return home
+      </Link>
+    </div>
+  );
 }
