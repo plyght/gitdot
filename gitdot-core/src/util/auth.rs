324c4726c331ebd7922d64428eddd44718290a5f
mikkel <mikkel@gitdot.io>
2026-05-25 16:25:46 -0400

    moved email templates under templates/email

--- gitdot-core/src/util/auth.rs
+++ gitdot-core/src/util/auth.rs
@@ -5,7 +5,7 @@ pub const S2_SERVER_ID: &str = "s2-server";
 
 pub const NOREPLY_EMAIL: &str = "gitdot <noreply@gitdot.io>";
 
-const CODE_TEMPLATE: &str = include_str!("../../templates/code.html");
+const CODE_TEMPLATE: &str = include_str!("../../templates/email/code.html");
 
 /// Reserved usernames that cannot be used for user or organization names.
 static RESERVED_NAMES: LazyLock<HashSet<&'static str>> = LazyLock::new(|| {
