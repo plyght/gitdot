16d9f33b8fa6b88495ab2985d0744842d40f59d0
baepaul <paul@gitdot.io>
2026-06-04 10:43:19 -0400

    fetch README in server side promise for repo index

--- gitdot-web/app/(main)/[owner]/[repo]/(index)/page.tsx
+++ gitdot-web/app/(main)/[owner]/[repo]/(index)/page.tsx
@@ -1,16 +1,11 @@
-import type { RepositoryBlobResource } from "gitdot-api";
 import {
   getCurrentUser,
   getRepository,
   getRepositoryActivity,
+  getRepositoryBlob,
 } from "gitdot-client";
-import { fetchResources } from "gitdot-dal/server";
-import { PageClient } from "./page.client";
+import { MarkdownBody } from "../ui/markdown/markdown-body";
 import { RepoPanel } from "./ui/repo-panel";
-
-export type Resources = {
-  readme: RepositoryBlobResource | null;
-};
 
 export default async function Page({
   params,
@@ -18,9 +13,10 @@ export default async function Page({
   params: Promise<{ owner: string; repo: string }>;
 }) {
   const { owner, repo } = await params;
-  const [repository, current] = await Promise.all([
+  const [repository, current, readme] = await Promise.all([
     getRepository(owner, repo),
     getCurrentUser(false),
+    getRepositoryBlob(owner, repo, { path: "README.md" }),
   ]);
   if (!repository) return null;
 
@@ -30,15 +26,19 @@ export default async function Page({
       (m) => m.name === owner && m.role === "admin",
     );
 
-  const resources = fetchResources({
-    readme: (p) => p.getBlob(owner, repo, "README.md"),
-  });
-
   const activityPromise = getRepositoryActivity(owner, repo);
 
   return (
     <div className="flex h-full w-full overflow-hidden">
-      <PageClient resources={resources} />
+      <div className="flex-1 min-w-0 overflow-y-auto scrollbar-none">
+        {readme ? (
+          <div className="p-4 w-full">
+            <MarkdownBody content={readme.content} />
+          </div>
+        ) : (
+          <div className="p-2 font-mono h-9 text-sm">README.md not found</div>
+        )}
+      </div>
       <RepoPanel
         repository={repository}
         activityPromise={activityPromise}
